9f82cf41a17e17264d39053844e44ee9b8e314a3
Sweekrithi Nayak K <sweekrithinayyyak@gmail.com>
2021-09-08 13:43:15 +0530

    stage 2.5

--- sketch.js
+++ sketch.js
@@ -15,10 +15,22 @@ var fruit_con;
 var bgImg, food, rabbitImg;
 var bunny;
 var button;
+
+var blink,eat,sad;
 function preload(){
   bgImg=loadImage("background.png");
   food=loadImage("melon.png");
   rabbitImg=loadImage("Rabbit-01.png");
+
+  blink=loadAnimation("blink_1.png","blink_2.png","blink_3.png");
+ eat=loadAnimation("eat_0.png","eat_1.png","eat_2.png","eat_3.png","eat_4.png");
+ sad=loadAnimation("sad_1.png","sad_2.png","sad_3.png");
+
+ blink.playing=true;
+ eat.playing=true;
+ eat.looping=false;
+
+
 
 }
 function setup() 
@@ -37,10 +49,14 @@ function setup()
   fruit=Bodies.circle(200,200,10)
   World.add(world,fruit);
   Matter.Composite.add( rope.body,fruit);
-  
+  blink.frameDelay=20;
+  eat.frameDealy=20;
   bunny=createSprite(250,650);
-  bunny.addImage(rabbitImg)
+ 
   bunny.scale=0.2;
+  bunny.addAnimation('blinking',blink);
+  bunny.addAnimation('eating',eat);
+  bunny.changeAnimation('blinking');
 
   fruit_con=new Link(rope,fruit);
   button=createImg('cut_button.png');
@@ -67,6 +83,7 @@ function drop(){
   rope.break();
   fruit_con.deattach();
   fruit_con=null;
+ // bunny.changeAnimation('eating')
 }
 
 
